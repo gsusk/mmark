eb4210b0c398cc0fc47dce15194ccea0d014e94c
gsus <satehortuamercado@gmail.com>
2026-03-08 18:15:13 -0500

    limpieza de imports

--- src/main/java/org/adso/minimarket/dto/RegisterRequest.java
+++ src/main/java/org/adso/minimarket/dto/RegisterRequest.java
@@ -1,7 +1,6 @@
 package org.adso.minimarket.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
