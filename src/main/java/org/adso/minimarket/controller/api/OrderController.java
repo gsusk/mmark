9adaf7aeec119ba1f1ee7ad59f2cb7a907dd35bf
gsus <satehortuamercado@gmail.com>
2026-03-16 23:42:06 -0500

    anadido check contra orderId malformadas

--- src/main/java/org/adso/minimarket/controller/api/OrderController.java
+++ src/main/java/org/adso/minimarket/controller/api/OrderController.java
@@ -1,12 +1,17 @@
 package org.adso.minimarket.controller.api;
 
 import org.adso.minimarket.config.UserPrincipal;
+import org.adso.minimarket.dto.OrderDetails;
 import org.adso.minimarket.dto.OrderSummary;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.List;
 
 public interface OrderController {
+    ResponseEntity<OrderDetails> placeOrder(@AuthenticationPrincipal UserPrincipal principal);
+
     ResponseEntity<OrderSummary> getOrderDetails(UserPrincipal principal, String orderId);
+
     ResponseEntity<List<OrderSummary>> getMyOrders(UserPrincipal principal);
 }
--- src/main/java/org/adso/minimarket/controller/api/OrderControllerImpl.java
+++ src/main/java/org/adso/minimarket/controller/api/OrderControllerImpl.java
@@ -1,17 +1,22 @@
 package org.adso.minimarket.controller.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.adso.minimarket.config.UserPrincipal;
+import org.adso.minimarket.dto.OrderDetails;
 import org.adso.minimarket.dto.OrderSummary;
+import org.adso.minimarket.exception.BadRequestException;
 import org.adso.minimarket.service.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 public class OrderControllerImpl implements OrderController {
     private final OrderService orderService;
@@ -20,13 +25,29 @@ public class OrderControllerImpl implements OrderController {
         this.orderService = orderService;
     }
 
+    @PostMapping("/orders")
+    @Override
+    public ResponseEntity<OrderDetails> placeOrder(
+            @AuthenticationPrincipal UserPrincipal principal) {
+
+        OrderDetails orderDetails = orderService.placeOrder(principal.getUser());
+        return ResponseEntity.ok(orderDetails);
+    }
+
     @Override
     @GetMapping("/orders/{order_id}")
     public ResponseEntity<OrderSummary> getOrderDetails(
             @AuthenticationPrincipal UserPrincipal principal,
             @PathVariable("order_id") String orderId) {
+        UUID parsedOrderId;
+        try {
+            parsedOrderId = UUID.fromString(orderId);
+        } catch (Exception e) {
+            log.error("Error parsing order id: {}", e.getMessage());
+            throw new BadRequestException("orderId invalida o malformada");
+        }
 
-        OrderSummary summary = orderService.getOrderById(UUID.fromString(orderId), principal.getId());
+        OrderSummary summary = orderService.getOrderById(parsedOrderId, principal.getId());
         return ResponseEntity.ok(summary);
     }
 
