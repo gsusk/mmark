77e2025799825d4061a67b322b37bab6d3a36fa9
gsus <satehortuamercado@gmail.com>
2026-03-16 23:28:22 -0500

    anadida query para productos del carrito eager

--- src/main/java/org/adso/minimarket/repository/jpa/CartRepository.java
+++ src/main/java/org/adso/minimarket/repository/jpa/CartRepository.java
@@ -4,15 +4,32 @@ import org.adso.minimarket.models.cart.Cart;
 import org.adso.minimarket.models.cart.CartStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Optional<Cart> findCartByUserIdAndStatus(Long userId, CartStatus status);
+    @Query("""
+                SELECT c FROM Cart c
+                LEFT JOIN FETCH c.cartItems ci
+                LEFT JOIN FETCH ci.product
+                WHERE c.guestId = :guestId AND c.status = :status
+            """)
+    Optional<Cart> findCartWithProductsByGuestIdAndStatus(UUID guestId, CartStatus status);
+
+    @Query("""
+                SELECT c FROM Cart c
+                LEFT JOIN FETCH c.cartItems ci
+                LEFT JOIN FETCH ci.product
+                WHERE c.user = :userId AND c.status = :status
+            """)
+    Optional<Cart> findCartWithProductsByUserIdAndStatus(Long userId, CartStatus status);
 
     Optional<Cart> findCartByGuestIdAndStatus(UUID guestId, CartStatus status);
+
+    Optional<Cart> findCartByUserIdAndStatus(Long userId, CartStatus status);
 
     List<Cart> findCartsByUserId(Long userId);
 
